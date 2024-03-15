099a4a58aac3326390b8adaa81956d20343749c2
Eray Sahin <eraysahinmoon@gmail.com>
2024-03-14 21:38:24 -0400

    removed unified topology and url parser (deprecated)

--- server/db.js
+++ server/db.js
@@ -5,10 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO);
         console.log('MongoDB connected successfully.');
     } catch (error) {
         console.error('MongoDB connection error:', error);
