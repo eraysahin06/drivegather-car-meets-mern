6ab8022e381b0c3b21dc922b2f40fcd3ad858a06
Eray Sahin <eraysahinmoon@gmail.com>
2024-03-15 17:43:33 -0400

    updated car data

--- client/src/data/carBrands.js
+++ client/src/data/carBrands.js
@@ -1,7 +1,14 @@
 const carBrands = {
-    "Ford": ["Fiesta", "Focus", "Mustang"],
-    "Toyota": ["Corolla", "Camry", "Prius"],
-    "Honda": ["Civic", "Accord", "CR-V"],
+    "Ford": ["Fiesta", "Focus", "Mustang", "Explorer", "F-150"],
+    "Toyota": ["Corolla", "Camry", "Prius", "RAV4", "Highlander"],
+    "Honda": ["Civic", "Accord", "CR-V", "Pilot", "Fit"],
+    "Chevrolet": ["Cruze", "Malibu", "Impala", "Equinox", "Silverado"],
+    "Nissan": ["Sentra", "Altima", "Maxima", "Rogue", "Murano"],
+    "Hyundai": ["Elantra", "Sonata", "Tucson", "Santa Fe", "Kona"],
+    "Volkswagen": ["Jetta", "Passat", "Golf", "Tiguan", "Atlas"],
+    "BMW": ["3 Series", "5 Series", "X3", "X5", "X7"],
+    "Mercedes-Benz": ["C-Class", "E-Class", "GLC", "GLE", "GLS"],
+    "Audi": ["A3", "A4", "A6", "Q5", "Q7"],
     // Add more brands and models
 };
 
