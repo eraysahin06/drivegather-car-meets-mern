3abfa955914c0f918341db30a18cf4f3850ece84
Eray Sahin <eraysahinmoon@gmail.com>
2024-03-15 13:59:49 -0400

    created about page

---
+++ client/src/pages/About/About.jsx
@@ -0,0 +1,25 @@
+
+const About = () => {
+    return (
+        <div className="container mx-auto px-4">
+            <h1 className="text-3xl font-bold text-center my-8">About Car Meets</h1>
+            <p className="text-lg">
+                Car Meets is a platform for car enthusiasts to connect and share their passion for cars. 
+                Whether you&apos;re a fan of classic cars, sports cars, or any other type of vehicle, Car Meets 
+                is the place to meet like-minded individuals, share your experiences, and learn from others.
+            </p>
+            <p className="text-lg mt-4">
+                Our mission is to bring together car lovers from all over the world and create a vibrant 
+                community where everyone can share their love for automobiles. We believe that cars are more 
+                than just a mode of transportation; they are a way of life.
+            </p>
+            <p className="text-lg mt-4">
+                Join us and be a part of our growing community of car enthusiasts. Share your car stories, 
+                participate in discussions, and connect with others who share your passion. Let&apos;s make Car Meets 
+                the ultimate destination for all things automotive!
+            </p>
+        </div>
+    );
+};
+
+export default About;
