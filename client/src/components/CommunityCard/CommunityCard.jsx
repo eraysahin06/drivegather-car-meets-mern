f5913278349aabee226dbd56eabaa4f3a86cc02e
Eray Sahin <eraysahinmoon@gmail.com>
2024-03-17 14:34:08 -0400

    design

--- client/src/components/CommunityCard/CommunityCard.jsx
+++ client/src/components/CommunityCard/CommunityCard.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
-import { FaUser } from 'react-icons/fa'; // Import the icon
+import { FaUser } from 'react-icons/fa';
 
 const CommunityCard = ({ community, isCreator }) => {
     return (
         <div className="bg-gray-800 rounded-md p-4 mb-4">
-            <h3 className="text-xl font-bold">{community.name} {isCreator && <FaUser title="Created by you" />}</h3>
+            <h3 className="text-xl font-bold">{isCreator && <FaUser className='p-1' title="Created by you" />}{community.name} </h3>
             <p>Type: {community.type}</p>
             <p>Creator: {community.creatorUsername}</p>
             <p>Members: {community.memberCount}</p>
