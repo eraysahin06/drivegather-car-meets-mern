d8aa8d5dba6b8b943302493a74c90ebecb50da14
Eray Sahin <eraysahinmoon@gmail.com>
2024-03-18 21:54:38 -0400

    added ferrari

--- client/src/data/carBrands.js
+++ client/src/data/carBrands.js
@@ -1,15 +1,24 @@
 const carBrands = {
-    "Ford": ["Fiesta", "Focus", "Mustang", "Explorer", "F-150"],
-    "Toyota": ["Corolla", "Camry", "Prius", "RAV4", "Highlander"],
-    "Honda": ["Civic", "Accord", "CR-V", "Pilot", "Fit"],
-    "Chevrolet": ["Cruze", "Malibu", "Impala", "Equinox", "Silverado"],
-    "Nissan": ["Sentra", "Altima", "Maxima", "Rogue", "Murano"],
-    "Hyundai": ["Elantra", "Sonata", "Tucson", "Santa Fe", "Kona"],
-    "Volkswagen": ["Jetta", "Passat", "Golf", "Tiguan", "Atlas"],
-    "BMW": ["3 Series", "5 Series", "X3", "X5", "X7"],
-    "Mercedes-Benz": ["C-Class", "E-Class", "GLC", "GLE", "GLS"],
-    "Audi": ["A3", "A4", "A6", "Q5", "Q7"],
-    // Add more brands and models
+  Ford: ["Fiesta", "Focus", "Mustang", "Explorer", "F-150"],
+  Toyota: ["Corolla", "Camry", "Prius", "RAV4", "Highlander"],
+  Honda: ["Civic", "Accord", "CR-V", "Pilot", "Fit"],
+  Chevrolet: ["Cruze", "Malibu", "Impala", "Equinox", "Silverado"],
+  Nissan: ["Sentra", "Altima", "Maxima", "Rogue", "Murano"],
+  Hyundai: ["Elantra", "Sonata", "Tucson", "Santa Fe", "Kona"],
+  Volkswagen: ["Jetta", "Passat", "Golf", "Tiguan", "Atlas"],
+  BMW: ["3 Series", "5 Series", "X3", "X5", "X7"],
+  "Mercedes-Benz": ["C-Class", "E-Class", "GLC", "GLE", "GLS"],
+  Audi: ["A3", "A4", "A5", "A6", "Q5", "Q7"],
+  Ferrari: [
+    "458 Italia",
+    "488 GTB",
+    "488 Pista",
+    "355",
+    "430",
+    "SF90",
+    "SF90 Spider",
+  ],
+  // Add more brands and models
 };
 
 export default carBrands;
