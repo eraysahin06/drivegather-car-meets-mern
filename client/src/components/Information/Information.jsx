1f0886a9bec6b78d955c7742610f960da85b7bb7
Eray Sahin <eraysahinmoon@gmail.com>
2024-03-23 10:28:28 -0400

    styling

--- client/src/components/Information/Information.jsx
+++ client/src/components/Information/Information.jsx
@@ -5,34 +5,34 @@ const Information = () => {
     <div className="flex flex-col items-center justify-center bg-white text-black p-8 rounded-md my-4 mx-4 min-h-[500px]">
       <h2 className="font-bold text-2xl mb-8 text-center">About DriveGather</h2>
       <div className="flex flex-wrap justify-center gap-4">
-        <div className="text-black border-2 border-black p-4 rounded-md shadow flex-1 max-w-md">
+        <div className="text-white bg-gray-700 border-2 border-gray-700 p-4 rounded-md shadow flex-1 max-w-md">
           <h3 className="font-bold text-xl mb-2 flex items-center">
             <FaCar className="mr-2" />
             Car Enthusiasts
           </h3>
-          <p className="text-md text-gray-800">
+          <p className="text-md">
             DriveGather is a platform dedicated to bringing car enthusiasts
             together. Discover local car meets, events, and communities where
             you can share your passion for cars with like-minded individuals.
           </p>
         </div>
-        <div className="text-black border-2 border-black p-4 rounded-md shadow flex-1 max-w-md">
+        <div className="text-white bg-gray-700 border-2 border-gray-700 p-4 rounded-md shadow flex-1 max-w-md">
           <h3 className="font-bold text-xl mb-2 flex items-center">
             <FaUsers className="mr-2" />
             Community Connection
           </h3>
-          <p className="text-md text-gray-800">
+          <p className="text-md">
             Join communities to connect with other car enthusiasts, discuss your
             favorite cars, and plan meet-ups. DriveGather is all about creating
             a vibrant community for car lovers.
           </p>
         </div>
-        <div className="text-black border-2 border-black p-4 rounded-md shadow flex-1 max-w-md">
+        <div className="text-white bg-gray-700 border-2 border-gray-700 p-4 rounded-md shadow flex-1 max-w-md">
           <h3 className="font-bold text-xl mb-2 flex items-center">
             <FaMapMarkerAlt className="mr-2" />
             Discover Events
           </h3>
-          <p className="text-md text-gray-800">
+          <p className="text-md">
             Explore events and car meets near you. Whether you&apos;re into
             classic cars, sports cars, or just want to check out some cool
             rides, DriveGather has something for everyone.
