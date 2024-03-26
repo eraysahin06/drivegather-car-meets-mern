00dd066a3ace9858a90a403d45db7b489f97cdac
Eray Sahin <eraysahinmoon@gmail.com>
2024-03-26 16:14:53 -0400

    view all -> View All Communities

--- client/src/components/Communities/YourCommunities.jsx
+++ client/src/components/Communities/YourCommunities.jsx
@@ -121,7 +121,7 @@ const Communities = () => {
         </div>
         <div className="flex items-center justify-center">
         <Link to='/communities' className="text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          View All
+          View All Communities
         </Link>
         </div>
        
