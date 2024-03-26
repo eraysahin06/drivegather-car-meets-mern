289d0e4db173129196c4d3ec765d4a9074dfc4a9
Eray Sahin <eraysahinmoon@gmail.com>
2024-03-26 15:02:28 -0400

    Updated community hero section

--- client/src/pages/AllCommunities/AllCommunities.jsx
+++ client/src/pages/AllCommunities/AllCommunities.jsx
@@ -1,13 +1,21 @@
 import Communities from "../../components/Communities/Communities";
-import MEET from '../../assets/meet.jpg';
+import MEET from "../../assets/meet.jpg";
 
 const AllCommunities = () => {
   return (
     <>
       <div className="relative">
-        <img src={MEET} alt="communities" className="w-full h-96 object-cover"  style={{ height: '75vh', filter: 'brightness(50%)' }} />
-        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
-          <h2 className="text-white text-5xl font-bold">Communities</h2>
+        <img
+          src={MEET}
+          alt="communities"
+          className="w-full h-96 object-cover"
+          style={{ height: "75vh", filter: "brightness(50%)" }}
+        />
+        <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center">
+          <div className="text-center text-white">
+            <h1 className="text-5xl font-bold mb-4">Communities</h1>
+            <p className="text-xl">Find the perfect community for you</p>
+          </div>
         </div>
       </div>
       <Communities />
