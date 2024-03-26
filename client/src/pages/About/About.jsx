b650036698cfdf6091ab53380268c403c0f0c957
Eray Sahin <eraysahinmoon@gmail.com>
2024-03-26 16:29:51 -0400

    Updated Profile Page

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,9 +1,9 @@
-import AboutImg from '../../assets/aboutimg.jpg';
+import AboutImg from "../../assets/aboutimg.jpg";
 
 const About = () => {
-    return (
-        <>
-          <div className="relative">
+  return (
+    <>
+      <div className="relative">
         <img
           src={AboutImg}
           alt="communities"
@@ -16,27 +16,30 @@ const About = () => {
           </div>
         </div>
       </div>
-            <div className="mx-auto px-4 py-8 max-w-4xl">
-                <h1 className="text-3xl font-bold text-center my-8">About Car Meets</h1>
-                <p className="text-lg leading-relaxed">
-                    Car Meets is a platform for car enthusiasts to connect and share their passion for cars. 
-                    Whether you&apos;re a fan of classic cars, sports cars, or any other type of vehicle, Car Meets 
-                    is the place to meet like-minded individuals, share your experiences, and learn from others.
-                </p>
-                <p className="text-lg leading-relaxed mt-4">
-                    Our mission is to bring together car lovers from all over the world and create a vibrant 
-                    community where everyone can share their love for automobiles. We believe that cars are more 
-                    than just a mode of transportation; they are a way of life.
-                </p>
-                <p className="text-lg leading-relaxed mt-4">
-                    Join us and be a part of our growing community of car enthusiasts. Share your car stories, 
-                    participate in discussions, and connect with others who share your passion. Let&apos;s make Car Meets 
-                    the ultimate destination for all things automotive!
-                </p>
-            </div>
-        </>
-       
-    );
+      <div className="mx-auto px-4 py-8 max-w-4xl">
+        <h1 className="text-3xl font-bold text-center my-8">About Car Meets</h1>
+        <p className="text-lg leading-relaxed">
+          Car Meets is a platform for car enthusiasts to connect and share their
+          passion for cars. Whether you&apos;re a fan of classic cars, sports
+          cars, or any other type of vehicle, Car Meets is the place to meet
+          like-minded individuals, share your experiences, and learn from
+          others.
+        </p>
+        <p className="text-lg leading-relaxed mt-4">
+          Our mission is to bring together car lovers from all over the world
+          and create a vibrant community where everyone can share their love for
+          automobiles. We believe that cars are more than just a mode of
+          transportation; they are a way of life.
+        </p>
+        <p className="text-lg leading-relaxed mt-4">
+          Join us and be a part of our growing community of car enthusiasts.
+          Share your car stories, participate in discussions, and connect with
+          others who share your passion. Let&apos;s make Car Meets the ultimate
+          destination for all things automotive!
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default About;
--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,80 +1,113 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useAuth } from '../../hooks/useAuth';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useAuth } from "../../hooks/useAuth";
+import ProfileImage from "../../assets/profileimg.jpg";
 
 const Profile = () => {
-    const authUser = useAuth();
-    const [user, setUser] = useState(null);
-    const [username, setUsername] = useState('');
-    const [isEditing, setIsEditing] = useState(false);
-    const [errorMessage, setErrorMessage] = useState('');
+  const authUser = useAuth();
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-    useEffect(() => {
-        const fetchUserDetails = async () => {
-            try {
-                const response = await axios.get(`${import.meta.env.VITE_HOST}/users/${authUser.email}`);
-                setUser(response.data);
-                setUsername(response.data.username || '');
-            } catch (error) {
-                console.error('Error fetching user details:', error);
-                setErrorMessage('Failed to fetch user details');
-            }
-        };
-
-        if (authUser) {
-            fetchUserDetails();
-        }
-    }, [authUser]);
-
-    const handleEdit = () => {
-        setIsEditing(true);
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_HOST}/users/${authUser.email}`
+        );
+        setUser(response.data);
+        setUsername(response.data.username || "");
+      } catch (error) {
+        console.error("Error fetching user details:", error);
+        setErrorMessage("Failed to fetch user details");
+      }
     };
 
-    const handleSave = async () => {
-        try {
-            const response = await axios.put(`${import.meta.env.VITE_HOST}/users/${user.email}`, {
-                username
-            });
-            if (response.data) {
-                setUser(response.data);
-                setUsername(response.data.username);
-            }
-            setIsEditing(false);
-        } catch (error) {
-            setErrorMessage('Error updating profile');
-        }
-    };
+    if (authUser) {
+      fetchUserDetails();
+    }
+  }, [authUser]);
 
-    return (
-        <div className="flex h-screen bg-white text-black">
-            <div className="flex-1 p-5">
-                <h1 className="text-2xl font-bold mb-4">Profile</h1>
-                {errorMessage && <div className="text-red-500">{errorMessage}</div>}
-                <div className="mb-4">
-                    <label className="block">Username</label>
-                    {isEditing ? (
-                        <input
-                            type="text"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            className="border p-2 text-black"
-                        />
-                    ) : (
-                        <p className="text-lg">{username}</p>
-                    )}
-                </div>
-                <div className="mb-4">
-                    <label className="block">Email</label>
-                    <p className="text-lg">{user?.email}</p>
-                </div>
-                {isEditing ? (
-                    <button onClick={handleSave} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Save</button>
-                ) : (
-                    <button onClick={handleEdit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit Username</button>
-                )}
-            </div>
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = async () => {
+    try {
+      const response = await axios.put(
+        `${import.meta.env.VITE_HOST}/users/${user.email}`,
+        {
+          username,
+        }
+      );
+      if (response.data) {
+        setUser(response.data);
+        setUsername(response.data.username);
+      }
+      setIsEditing(false);
+    } catch (error) {
+      setErrorMessage("Error updating profile");
+    }
+  };
+
+  return (
+    <div className="flex flex-col min-h-screen bg-white text-black">
+      <div className="relative">
+        <img
+          src={ProfileImage}
+          alt="Profile"
+          className="w-full object-cover"
+          style={{ height: "75vh", filter: "brightness(50%)" }}
+        />
+        <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center">
+          <div className="text-center text-white">
+            <h1 className="text-5xl font-bold mb-4">Your Profile</h1>
+          </div>
         </div>
-    );
+      </div>
+      <div className="flex-1 flex items-center justify-center p-8">
+        <div className="bg-white shadow-md rounded-lg p-6 max-w-md w-full">
+          <h1 className="text-2xl font-bold mb-4">Profile</h1>
+          {errorMessage && (
+            <div className="text-red-500 mb-4">{errorMessage}</div>
+          )}
+          <div className="mb-4">
+            <label className="block font-semibold">Username</label>
+            {isEditing ? (
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="border p-2 text-black w-full rounded-md"
+              />
+            ) : (
+              <p className="text-lg">{username}</p>
+            )}
+          </div>
+          <div className="mb-4">
+            <label className="block font-semibold">Email</label>
+            <p className="text-lg">{user?.email}</p>
+          </div>
+          {isEditing ? (
+            <button
+              onClick={handleSave}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Save
+            </button>
+          ) : (
+            <button
+              onClick={handleEdit}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Edit Username
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
